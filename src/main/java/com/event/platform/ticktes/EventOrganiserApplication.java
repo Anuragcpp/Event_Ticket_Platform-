b962a47a3e52761acab1e67c3e4dec1450599053
Anurag Sen <anuragsen@Anurags-MacBook-Pro.local>
2026-03-04 17:54:43 +0530

    Initial Commit

---
+++ src/main/java/com/event/platform/ticktes/EventOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.event.platform.ticktes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventOrganiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/platform/ticktes/EventOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.platform.ticktes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrganiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
